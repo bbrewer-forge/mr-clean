98b30f75083fd94b75f2b4af525579d478c10e15
Benjamin Brewer <ben@benbrewer.me>
2020-08-20 11:02:16 -0400

    initial commit

---
+++ src/main/java/com/smartcolumbusos/mrclean/MrcleanApplication.java
@@ -0,0 +1,13 @@
+package com.smartcolumbusos.mrclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrcleanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MrcleanApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/smartcolumbusos/mrclean/MrcleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcolumbusos.mrclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrcleanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
